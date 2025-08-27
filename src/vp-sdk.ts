65bcd0cec0272d7a601668013ad19e02e34e3756
Nelson Fulfil <rockyxcoded@gmail.com>
2025-08-27 16:59:10 +0100

    feat: add vp-sdk.ts

---
+++ src/vp-sdk.ts
@@ -0,0 +1,49 @@
+import axios from "./axios";
+import { AgentsClient } from "./client/AgentsClient";
+import { AnalyticsClient } from "./client/AnalyticsClient";
+import { APIKeysClient } from "./client/APIKeysClient";
+import { AuthClient } from "./client/AuthClient";
+import { CreditsClient } from "./client/CreditsClient";
+import { NotificationsClient } from "./client/NotificationsClient";
+import { RAGClient } from "./client/RAGClient";
+import { STTClient } from "./client/STTClient";
+import { TTSClient } from "./client/TTSClient";
+import { UsersClient } from "./client/UsersClient";
+import { VoicesClient } from "./client/VoicesClient";
+import type { SdkConfig } from "./types";
+import { HttpClient } from "./utils/http";
+
+export class VpSdk {
+  private httpClient: HttpClient;
+  public auth: AuthClient;
+  public notifications: NotificationsClient;
+  public agents: AgentsClient;
+  public voices: VoicesClient;
+  public users: UsersClient;
+  public credits: CreditsClient;
+  public admin: AnalyticsClient;
+  public stt: STTClient;
+  public health: RAGClient;
+  public apiKey: APIKeysClient;
+  public tts: TTSClient;
+
+  constructor(httpClient?: HttpClient, config?: SdkConfig) {
+    this.httpClient = httpClient ?? new HttpClient(axios, config);
+
+    this.auth = new AuthClient(this.httpClient);
+    this.notifications = new NotificationsClient(this.httpClient);
+    this.agents = new AgentsClient(this.httpClient);
+    this.voices = new VoicesClient(this.httpClient);
+    this.users = new UsersClient(this.httpClient);
+    this.credits = new CreditsClient(this.httpClient);
+    this.admin = new AnalyticsClient(this.httpClient);
+    this.stt = new STTClient(this.httpClient);
+    this.health = new RAGClient(this.httpClient);
+    this.apiKey = new APIKeysClient(this.httpClient);
+    this.tts = new TTSClient(this.httpClient);
+  }
+
+  setAuthToken(token: string) {
+    this.httpClient.setAuthToken(token);
+  }
+}
